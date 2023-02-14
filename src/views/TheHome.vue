9af225ee209e922da19fb36174f6ed6290e09e1a
Leandro Cristino <leo_cristino@yahoo.com.br>
2023-02-13 23:55:16 -0300

    para novo pr

--- src/views/TheHome.vue
+++ src/views/TheHome.vue
@@ -10,7 +10,10 @@
         <label for="password">Password</label>
         <input type="password" class="login-container__input login-container__input-password" placeholder="Password" autocomplete="off" v-model="password"/>
         <div>
-          <span class="question">Don't have an account yet?</span><span class="question"><router-link :to='{ name: "TheRegister"}' class="sign">Sign up here</router-link></span>
+          <span class="question">Don't have an account yet?</span>
+          <span class="question">
+            <a href="#" @click="toogleIsLogin(false)" class="sign">Sign up here</a>
+          </span>
         </div>
         <button type="submit" class="login-container__submit" @click.prevent="submitForm">Login</button>
       </form>
@@ -20,10 +23,13 @@
         <label for="email">Email</label>
         <input type="text" class="login-container__input" autocomplete="off" placeholder="email@email.com" id="email" v-model="username"/>
         <label for="password">Password</label>
-        <input type="password" class="login-container__input" placeholder="Password" autocomplete="off" v-model="password"/>
-      
-        <button type="submit" class="login-container__submit" @click.prevent="submitForm">Login</button>
-        <router-link :to='{ name: "TheHome"}'   class="back-to-login">Back to Login</router-link>
+        <input type="password" class="login-container__input login-container__input-password" placeholder="Password" autocomplete="off" v-model="password"/>
+        <button type="submit" class="login-container__register login-container__submit" @click.prevent="submitForm">Register</button>
+        <div class="login-form__div-back-to-login">
+          <a href="#" @click="toogleIsLogin(true)"  class="back-to-login">Back to Login</a>
+        </div>
+
+
       </form>
     </div>
   </div>
@@ -33,22 +39,27 @@
 export default {
   data() {
     return {
+      isLogin : true,
       username: '',
       password: ''
     };
   },
 
+  mounted() {
+  },
+
   computed: {
-    isLogin(){
-      return this.$route.name == 'TheHome'
-    }
+   
   },
 
   methods: {
     submitForm() {
       console.log(`Username: ${this.username} Password: ${this.password}`);
       // Aqui você pode enviar as informações do formulário para o seu back-end
-    }
+    },
+    toogleIsLogin(status){
+      this.isLogin = status
+    },
   }
 };
 </script>
@@ -88,6 +99,10 @@ export default {
     border-radius: 10px;
    
   }
+
+  .login-form__div-back-to-login{
+    margin-top: 25px;
+  }
   
   .question{
     float: left;
@@ -101,7 +116,7 @@ export default {
   }
 
   .question > a{
-    color: #2E3188;
+    color: mediumpurple;
 
   }
 
@@ -129,6 +144,9 @@ export default {
     font-weight: bold;
     margin-top: 30px;
   } 
+  .login-container__register{
+    display: block;
+  }
   .back-to-login{
 		color: white;
 		font-size: 13px;
