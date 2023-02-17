b5befb815bb6d6057d3363c867b41f99293335b9
Leandro Cristino <leo_cristino@yahoo.com.br>
2023-02-16 21:43:33 -0300

    para novo pr

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import {createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
     { path: '', name: 'TheHome', component: () => import('../views/TheHome')},
-    // { path: '/register', name: 'TheRegister', component: () => import('../views/TheHome')}
 ]
 
 const router = createRouter({
--- src/views/TheHome.vue
+++ src/views/TheHome.vue
@@ -1,5 +1,5 @@
 <template class="container">
-  <div class="autenthication-container">
+  <div class="login-container">
     <div class="child">
       <h1 class="login-container__title">Vue Invoices App</h1>
 
@@ -12,21 +12,21 @@
         <div>
           <span class="question">Don't have an account yet?</span>
           <span class="question">
-            <a href="#" @click="toogleIsLogin(false)" class="sign">Sign up here</a>
+            <a @click="toogleIsLogin" class="sign">Sign up here</a>
           </span>
         </div>
         <button type="submit" class="login-container__submit" @click.prevent="submitForm">Login</button>
       </form>
 
       <form v-else autocomplete="off">
-        <p>Login</p>
+        <p>Register</p>
         <label for="email">Email</label>
         <input type="text" class="login-container__input" autocomplete="off" placeholder="email@email.com" id="email" v-model="username"/>
         <label for="password">Password</label>
         <input type="password" class="login-container__input login-container__input-password" placeholder="Password" autocomplete="off" v-model="password"/>
         <button type="submit" class="login-container__register login-container__submit" @click.prevent="submitForm">Register</button>
-        <div class="login-form__div-back-to-login">
-          <a href="#" @click="toogleIsLogin(true)"  class="back-to-login">Back to Login</a>
+        <div class="login-container__back-to-login-wrapper">
+          <a @click="toogleIsLogin"  class="back-to-login">Back to Login</a>
         </div>
 
 
@@ -45,39 +45,36 @@ export default {
     };
   },
 
-  mounted() {
-  },
-
-  computed: {
-   
-  },
-
   methods: {
     submitForm() {
       console.log(`Username: ${this.username} Password: ${this.password}`);
       // Aqui você pode enviar as informações do formulário para o seu back-end
     },
-    toogleIsLogin(status){
-      this.isLogin = status
+    toogleIsLogin(){
+      this.isLogin = !this.isLogin
     },
   }
 };
 </script>
 
 <style>
-
+ 
   .login-container__title{
     margin-bottom: 40px;
     color: white;
   }
 
-  .autenthication-container {
+  .login-container {
     height: 100vh;
     display: flex;
     flex-direction: row;
     justify-content: center;
     align-items: center;
     background-color: #141625;
+  }
+
+  .sign {
+    cursor:pointer;
   }
 
 	form > p{
@@ -100,7 +97,7 @@ export default {
    
   }
 
-  .login-form__div-back-to-login{
+  .login-container__back-to-login-wrapper{
     margin-top: 25px;
   }
   
@@ -150,5 +147,6 @@ export default {
   .back-to-login{
 		color: white;
 		font-size: 13px;
+    cursor: pointer;
 	}
 </style>
